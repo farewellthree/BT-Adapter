ff16ca1af2e69b3e1310635206ffb76fdbd1ce27
/bin/eash <1176569545@qq.com>
2024-02-02 10:13:44 +0800

    Installation Info

--- llava_base/scripts/install.sh
+++ llava_base/scripts/install.sh
@@ -1,8 +1,5 @@
 pip install -r requirements.txt
-#pip install flash-attn==1.0.7 --no-build-isolation
 pip install -U openmim
 mim install mmengine
-mim install mmcv
-cd ../../transformers
-pip install -e .
-cd ../../Video-ChatGPT/
+mim install mmcv==2.0.1
+
